c0c9108c952b10dba21c20d60e6363dc73bce128
Kevin Cui <krazycavin@gmail.com>
2017-04-09 13:44:00 +0200

    Update sidebar animation

--- static/js/main.js
+++ static/js/main.js
@@ -24,15 +24,11 @@ $(document).ready(function() {
 
   $('.btn-mobile-menu').click(function() {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
-    $('.btn-mobile-menu__icon').toggleClass('hidden');
-    $('.btn-mobile-close__icon').toggleClass('hidden');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 
   $('.navigation-wrapper .blog-button').click(function() {
     $('.navigation-wrapper').toggleClass('visible');
-    $('.btn-mobile-menu__icon').toggleClass('hidden');
-    $('.btn-mobile-close__icon').toggleClass('hidden');
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn');
   });
 });
-
-
