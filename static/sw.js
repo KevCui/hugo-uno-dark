4a753cfc53424c508626958065913011d41c10b7
Kevin Cui <krazycavin@gmail.com>
2018-12-31 17:58:46 +0100

    Improve PWA

--- static/sw.js
+++ static/sw.js
@@ -1,29 +1,51 @@
-self.addEventListener('install', e => {
-  let timeStamp = Date.now();
-  e.waitUntil(
-    caches.open('kevin-blog').then(cache => {
-      return cache.addAll([
-        `/`,
-        `/#blog`,
-        `/css/font.min.css`,
-        `/css/animate.min.css`,
-        `/css/monokai.min.css`,
-        `/js/umbrella.min.js`,
-        `/js/highlight.min.js`
-      ])
-      .then(() => self.skipWaiting());
-    })
-  )
-});
+var CACHE_NAME = 'kevin-site';
+var urlsToCache = [
+    '/',
+    '/#blog',
+    '/css/font.min.css',
+    '/css/animate.min.css',
+    '/css/monokai.min.css',
+    '/js/umbrella.min.js',
+    '/js/highlight.min.js'
+];
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch:true}).then(response => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
